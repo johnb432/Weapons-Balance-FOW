04649c1317cae939acfc47ac21429ab36d7ed2e6
johnb432 <john.biselx@gmail.com>
2020-12-24 18:38:28 +0100

    Initial commit

---
+++ addons/fix_FOW/CfgMagazineWells.hpp
@@ -0,0 +1,119 @@
+class CfgMagazineWells {
+    class CBA_3006_Spring {
+        WW2FixFOW[] = {
+            "fow_5Rnd_762x63"
+        };
+    };
+
+    class CBA_3006_BAR {
+        WW2FixFOW[] = {
+            "fow_20Rnd_762x63"
+        };
+    };
+
+    class CBA_3006_Belt {
+        WW2FixFOW[] = {
+            "fow_30Rnd_762x63",
+            "fow_50Rnd_762x63"
+        };
+    };
+
+    class CBA_3006_Garand {
+        WW2FixFOW[] = {
+            "fow_8Rnd_762x63"
+        };
+    };
+
+    class CBA_30Carbine_M1Carbine {
+        WW2FixFOW[] = {
+            "fow_15Rnd_762x33"
+        };
+    };
+
+    class CBA_303B_BREN {
+        WW2FixFOW[] = {
+            "fow_30Rnd_303_bren"
+        };
+    };
+
+    class CBA_303B_LeeEn {
+        WW2FixFOW[] = {
+            "fow_10Rnd_303"
+        };
+    };
+
+    class CBA_45ACP_Thompson_Stick {
+        WW2FixFOW[] = {
+            "fow_30Rnd_45acp",
+            "fow_30Rnd_45acp_T",
+        };
+    };
+
+    class CBA_45ACP_Grease {
+        WW2FixFOW[] = {
+            "fow_30Rnd_45acp",
+            "fow_30Rnd_45acp_T"
+        };
+    };
+
+    class CBA_45ACP_Reising {
+        WW2FixFOW[] = {
+            "fow_20Rnd_45acp"
+        };
+    };
+    class CBA_77x58_Arisaka {
+        WW2FixFOW[] = {
+            "fow_5Rnd_77x58"
+        };
+    };
+
+    class CBA_77x58_Type99 {
+        WW2FixFOW[] = {
+            "fow_30Rnd_77x58"
+        };
+    };
+    class CBA_792x33_StG {
+        WW2FixFOW[] = {
+            "fow_30Rnd_792x33"
+        };
+    };
+
+    class CBA_792x57_G43 {
+        WW2FixFOW[] = {
+            "fow_10nd_792x57"
+        };
+    };
+
+    class CBA_792x57_K98 {
+        WW2FixFOW[] = {
+            "fow_5Rnd_792x57"
+        };
+    };
+
+    class CBA_792x57_LINKS {
+        WW2FixFOW[] = {
+            "fow_50Rnd_792x57"
+        };
+    };
+
+    class CBA_9x19_MP40 {
+        WW2FixFOW[] = {
+            "fow_32Rnd_9x19_mp40",
+            "fow_32Rnd_9x19_sten"
+        };
+    };
+
+    class CBA_9x19_STEN {
+        WW2FixFOW[] = {
+            "fow_32Rnd_9x19_mp40",
+            "fow_32Rnd_9x19_sten"
+        };
+    };
+
+    class CBA_PIAT {
+        WW2FixFOW[] = {
+            "fow_1Rnd_piat_HE",
+            "fow_1Rnd_piat_HEAT"
+        };
+    };
+};
---
+++ addons/fix_FOW/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches {
+    class fixFOW {
+        name = "Fix FOW";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.96;
+        requiredAddons[] = {
+            "fow_weapons_c"
+        };
+        author = "FK Gaming";
+        authors[] = {
+            "johnb43"
+        };
+    };
+};
+
+//#include "CfgWeapons.hpp"
+//#include "CfgMagazines.hpp"
+#include "CfgMagazineWells.hpp"
